7c531146fb8b4e082bc82140baf94d0c655f37d8
Konrad Krenzlin <git@krenzlin.com>
2019-11-27 22:25:35 +0100

    Build for STM32F466

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+#[allow(unused)]
+use panic_halt;
+
+use cortex_m_rt;
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    loop {}
 }
