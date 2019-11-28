4df6b1b65a243d956c7c730308f0a53a397756c1
Konrad Krenzlin <git@krenzlin.com>
2019-11-28 22:05:25 +0100

    Add comments explaining the HAL functions

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,16 @@ use panic_halt;
 
 use cortex_m_rt as rt;
 use stm32f4xx_hal as hal;
-use crate::hal::prelude::*;
+use hal::prelude::*; // need for the GpioExt trait (-> .split)
 
 #[rt::entry]
 fn main() -> ! {
     if let Some(peripherals) = hal::stm32::Peripherals::take() {
-        let gpioa = peripherals.GPIOA.split();
+        let gpioa = peripherals.GPIOA.split(); // + sets RCC->AHB1ENR GPIOA bit
+        // .into_push_pull_output performs three steps
+        // 1) set PUPDR: 00 -> no pull-up, no pull-down
+        // 2) set OTYPER: 0 -> output push-pull
+        // 3) set MODER: 01 -> general purpose output mode
         let mut led = gpioa.pa5.into_push_pull_output();
         led.set_high().unwrap();
     }
