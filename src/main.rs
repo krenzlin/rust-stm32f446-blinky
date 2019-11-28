8b164cdcd77dbe480e19b9167d3e0b8867d117a9
Konrad Krenzlin <git@krenzlin.com>
2019-11-28 21:13:55 +0100

    Turn on user led

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
 #[allow(unused)]
 use panic_halt;
 
-use cortex_m_rt;
+use cortex_m_rt as rt;
+use stm32f4xx_hal as hal;
+use crate::hal::prelude::*;
 
-#[cortex_m_rt::entry]
+#[rt::entry]
 fn main() -> ! {
+    if let Some(peripherals) = hal::stm32::Peripherals::take() {
+        let gpioa = peripherals.GPIOA.split();
+        let mut led = gpioa.pa5.into_push_pull_output();
+        led.set_high().unwrap();
+    }
     loop {}
 }
